7cd6f5556ab369dea3d466771f0c4cb6f57653e7
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-04-04 20:41:25 -0400

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/saiteja/phase2_ws/src'
-whitelisted_packages = 'a_star'.split(';') if 'a_star' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/saiteja/phase2_ws/devel;/home/saiteja/catkin_ws/devel;/home/saiteja/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/saiteja/phase2_ws/devel;/home/saiteja/catkin_ws/devel;/home/saiteja/ws_moveit/devel;/opt/ros/noetic' != '' else []
