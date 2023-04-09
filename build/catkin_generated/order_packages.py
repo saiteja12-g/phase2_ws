6c6e6f7dd6cdb7da1277fdbd7219a0763d92743f
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-04-09 18:15:10 -0400

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/saiteja/phase2_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/saiteja/catkin_ws/devel;/home/saiteja/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/saiteja/catkin_ws/devel;/home/saiteja/ws_moveit/devel;/opt/ros/noetic' != '' else []
--- build/turtlebot3_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saiteja/phase2_ws/src/turtlebot3_msgs
-# Build directory: /home/saiteja/phase2_ws/build/turtlebot3_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
